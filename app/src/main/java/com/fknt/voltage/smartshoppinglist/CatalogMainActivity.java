e833650c2860a770ec57f7355f9516f1801c0387
Pavel Ferdman <p.ferdman@anc.net>
2017-02-10 17:15:19 +0200

    DB Schema review

---
+++ app/src/main/java/com/fknt/voltage/smartshoppinglist/CatalogMainActivity.java
@@ -0,0 +1,22 @@
+package com.fknt.voltage.smartshoppinglist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+/**
+ * Created by SD on 10.02.2017.
+ */
+
+public class CatalogMainActivity extends AppCompatActivity {
+
+    ListView lvCatalogsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog_main);
+
+    }
+}
